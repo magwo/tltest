2469b006b99ace90d6dd121e239be04b8fe48ea6
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2016-01-06 15:52:21 +0100

    Autosync

--- public/index.js
+++ public/index.js
@@ -4,6 +4,12 @@ $(function() {
   setInterval(function() {
     $.get("/traffic/", function(body) {
       console.log("Body:", body);
+      var value = parseFloat(body);
+      if(value > 0.0) {
+        $("body").removeClass("red").addClass("green");
+      } else {
+        $("body").removeClass("green").addClass("red");
+      }
     });
   }, 200);
 });
