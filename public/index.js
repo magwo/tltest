9a3379d53b84994739b723733a1749ada69c5329
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2016-01-06 15:53:32 +0100

    Autosync

--- public/index.js
+++ public/index.js
@@ -5,6 +5,7 @@ $(function() {
     $.get("/traffic/", function(body) {
       console.log("Body:", body);
       var value = parseFloat(body);
+      $(".displayer").text(value);
       if(value > 0.0) {
         $("html, body").removeClass("red").addClass("green");
       } else {
